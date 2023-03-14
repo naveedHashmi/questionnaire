79c62f9e706dc95d1b03af040be27fed7a2d14d7
Muhammad Naveed <mnaveed47012@gmail.com>
2023-03-14 05:02:15 +0500

    Change commented code in migration

--- db/migrate/20230313235050_add_time_stamps_to_questionnaires.rb
+++ db/migrate/20230313235050_add_time_stamps_to_questionnaires.rb
@@ -1,12 +1,12 @@
 class AddTimeStampsToQuestionnaires < ActiveRecord::Migration[6.1]
   def up
-    # add_column :questionnaires, :created_at, :datetime
-    # add_column :questionnaires, :updated_at, :datetime
+    add_column :questionnaires, :created_at, :datetime
+    add_column :questionnaires, :updated_at, :datetime
 
-    # Questionnaire.update_all(created_at: Time.current, updated_at: Time.current)
+    Questionnaire.update_all(created_at: Time.current, updated_at: Time.current)
 
-    # change_column :questionnaires, :created_at, :datetime, null: false
-    # change_column :questionnaires, :updated_at, :datetime, null: false
+    change_column :questionnaires, :created_at, :datetime, null: false
+    change_column :questionnaires, :updated_at, :datetime, null: false
   end
 
   def def down
